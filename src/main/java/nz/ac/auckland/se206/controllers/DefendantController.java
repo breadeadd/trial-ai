06a907872167afa1964136cf5604ed95cdb39c8f
Caitlin <caitlinkuan49@gmail.com>
2025-09-22 13:21:13 +1200

    Button interactable working

--- src/main/java/nz/ac/auckland/se206/controllers/DefendantController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/DefendantController.java
@@ -9,6 +9,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
 // import javafx.animation.KeyFrame;
 // import javafx.animation.Timeline;
 // import javafx.event.ActionEvent;
@@ -27,6 +29,14 @@ public class DefendantController extends ChatController {
 
   @FXML private ImageView flashbackSlideshow;
   @FXML private Button nextButton;
+  @FXML private Rectangle button1;
+  @FXML private Rectangle button2;
+  @FXML private Rectangle button3;
+  @FXML private Rectangle button4;
+  @FXML private ImageView btn1img;
+  @FXML private ImageView btn2img;
+  @FXML private ImageView btn3img;
+  @FXML private ImageView btn4img;
 
   /**
    * Initializes the chat view.
@@ -38,6 +48,7 @@ public class DefendantController extends ChatController {
     // Any required initialization code can be placed here
     loadImages(null);
     initChat();
+    initButtons();
 
     btnSend.setVisible(false);
     txtInput.setVisible(false);
@@ -130,6 +141,44 @@ public class DefendantController extends ChatController {
       btnSend.setVisible(true);
       txtInput.setVisible(true);
       txtaChat.setVisible(true);
+
+      button1.setVisible(true);
+      button2.setVisible(true);
+      button3.setVisible(true);
+      button4.setVisible(true);
     }
+  }
+
+  // Memory elements
+  // button initialisation
+  private void initButtons() {
+    button1.setVisible(false);
+    button2.setVisible(false);
+    button3.setVisible(false);
+    button4.setVisible(false);
+    btn1img.setVisible(false);
+    btn2img.setVisible(false);
+    btn3img.setVisible(false);
+    btn4img.setVisible(false);
+  }
+
+  @FXML
+  private void button1Clicked(MouseEvent event) throws IOException {
+    btn1img.setVisible(!btn1img.isVisible());
+  }
+
+  @FXML
+  private void button2Clicked(MouseEvent event) throws IOException {
+    btn2img.setVisible(!btn2img.isVisible());
+  }
+
+  @FXML
+  private void button3Clicked(MouseEvent event) throws IOException {
+    btn3img.setVisible(!btn3img.isVisible());
+  }
+
+  @FXML
+  private void button4Clicked(MouseEvent event) throws IOException {
+    btn4img.setVisible(!btn4img.isVisible());
   }
 }
